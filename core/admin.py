18e9140318739fdeb0d8f2761cb7a600bc2673ff
SoftLife0 <onikosiadewale18@gmail.com>
2024-09-01 09:19:04 +0000

    registering my models

--- core/admin.py
+++ core/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+admin.site.register(CourseAllocation)
+admin.site.register(Claim)
+admin.site.register(ClaimSession)
